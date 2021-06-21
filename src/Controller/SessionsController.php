52cee58577927577a3b949572f62593e46a9ab77
Marc Samtleben <marcsa1996@googlemail.com>
2021-06-21 22:12:24 +0200

    Add login

--- src/Controller/SessionsController.php
+++ src/Controller/SessionsController.php
@@ -2,10 +2,12 @@
 
 namespace App\Controller;
 
+use App\Repository\UserRepository;
 use Psr\Log\LoggerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Annotation\Route;
 
 class SessionsController extends AbstractController
@@ -21,11 +23,17 @@ class SessionsController extends AbstractController
     /**
      * @Route("/login", name="create_session", methods={"POST"})
      */
-    public function create(LoggerInterface $logger, Request $request): Response
+    public function create(Request $request, UserRepository $userRepository, UserPasswordHasherInterface $passwordHasher): Response
     {
         $email = $request->request->get('email');
         $password = $request->request->get('password');
 
-        return $this->json(['test' => 'toast']);
+        $user = $userRepository->findOneBy(['email' => $email]);
+        if ($user && $passwordHasher->isPasswordValid($user, $password)) {
+            return $this->json('Valid E-Mail or Password!');
+        }
+
+        $this->addFlash('alert', 'Ungültige E-Mail oder Passwort.');
+        return $this->redirectToRoute('new_session');
     }
 }
