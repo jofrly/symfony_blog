9290cf8662d1a7e863ea71d5af2169530374bbd2
Marc Samtleben <marcsa1996@googlemail.com>
2021-06-21 20:34:50 +0200

    Add user entity

---
+++ src/Entity/User.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\UserRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=UserRepository::class)
+ * @ORM\Table(name="`user`")
+ */
+class User
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $email;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $password_digest;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getEmail(): ?string
+    {
+        return $this->email;
+    }
+
+    public function setEmail(string $email): self
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    public function getPasswordDigest(): ?string
+    {
+        return $this->password_digest;
+    }
+
+    public function setPasswordDigest(string $password_digest): self
+    {
+        $this->password_digest = $password_digest;
+
+        return $this;
+    }
+}
